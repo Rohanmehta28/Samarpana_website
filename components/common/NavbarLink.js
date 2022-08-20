93491df3a2ca49263e7253d8660a2c2b04412006
Shreyas Kaundinya <shreyassk08@gmail.com>
2022-08-20 22:57:39 +0530

    Updated navbar with underline effect

--- components/common/NavbarLink.js
+++ components/common/NavbarLink.js
@@ -5,14 +5,15 @@ function NavbarLink(props) {
   const router = useRouter()
 
   return (
-    <div className={router.pathname == props.path ? 'active' : ''}>
-      <Link href={props.path} smooth={true} offset={50} duration={500}>
-        <div className=""></div>
-        <a className="cursor-pointer link link-underline link-underline-black px-3 py-2 text-md">
-          {props.title}
-        </a>
-      </Link>
-    </div>
+    <Link href={props.path} smooth={true} offset={50} duration={500}>
+      <a
+        className={`cursor-pointer link px-3 py-2 text-md ${
+          router.pathname == props.path ? 'active' : ''
+        }`}
+      >
+        {props.title}
+      </a>
+    </Link>
   )
 }
 
--- components/common/NavbarLinkMobile.js
+++ components/common/NavbarLinkMobile.js
@@ -5,15 +5,13 @@ function NavbarLinkMobile(props) {
   const router = useRouter()
 
   return (
-    <div className={router.pathname == props.path ? 'active' : ''}>
-      <Link
-        href={props.path}
-        smooth={true}
-        offset={50}
-        duration={500}
-        className="cursor-pointer at block px-3 py-2 rounded-md text-step-0 font-medium"
-      >
-        <a className="cursor-pointer at block px-3 py-2 rounded-md text-step-0 font-medium">
+    <div>
+      <Link href={props.path} smooth={true} offset={50} duration={500}>
+        <a
+          className={`cursor-pointer link block px-3 py-2 rounded-md text-step-0 font-medium ${
+            router.pathname == props.path ? 'active' : ''
+          }`}
+        >
           {props.title}
         </a>
       </Link>
