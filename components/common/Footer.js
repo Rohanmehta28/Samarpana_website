b6bf5c58e16208944c3e0a9914f4b5136f449d01
Rohan Nandan <rohannandansjv@gmail.com>
2022-08-25 18:47:47 +0530

    Events page done

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -1,13 +1,13 @@
 function Footer() {
   return (
     <div className="block relative pt-20 md:pt-0 bg-black">
-      <footer className="md:-bottom-0 w-full inset-x-0 bg-opacity-100 md:bg-opacity-20 bg-black py-20 md:py-8">
+      <footer className="lg: bottom-0 md:-bottom-0 w-full inset-x-0 bg-opacity-100 md:bg-opacity-20 bg-black py-20 md:py-8">
         <div className="flex flex-col gap-8 md:flex-row justify-center items-center font-red-hat">
           <div className="flex flex-col gap-2 text-base text-white">
             <p>PES UNIVERSITY</p>
             <p>100 Feet Ring Road, BSK III Stage,</p>
             <p>Bangalore-560085</p>
-            <p className="text-gray-400">{`AT'22 Tech and Design Team`}</p>
+            <p className="text-gray-400">{`Samarpana'22 Tech and Design Team`}</p>
           </div>
           <div className="flex flex-col gap-2 text-base text-white">
             <p>Contact Us :</p>
--- pages/events/index.js
+++ pages/events/index.js
@@ -3,15 +3,130 @@ import Page from '../../components/common/Page'
 
 export default function Events() {
   return (
-    <div>
+    <>
       <Head>
         <title>Samarpana 2022 | Events</title>
         <meta name="description" content="" />
         <link rel="icon" href="/at22_icon.ico" />
       </Head>
       <Page nopadding>
-        <h1>Events</h1>
+  
+      <div class="grid lg:justify-items-center lg:grid-cols-3 pt-9 md:grid-cols-2 ">
+        {/* Card 1 */}
+        <div class="max-w-xs mx-8 mb-16 rounded-2xl shadow-lg hover:shadow-2xl hover:scale-105">
+        <img class="w-full h-48 rounded-t-2xl"
+            src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+            alt="product" />
+        <div class="px-6 py-4">
+            <h4 class="mb-3 text-xl font-semibold tracking-tight text-gray-800">This is the title</h4>
+            <p class="leading-normal text-gray-700">Lorem ipsum dolor, sit amet cons ectetur adipis icing
+                elit.
+                Praesen tium, quibusdam facere quo laborum maiores sequi nam tenetur laud.
+                Praesen tium, quibusdam facere quo laborum maiores sequi nam tenetur laud.
+                Praesen tium, quibusdam facere quo laborum maiores sequi nam tenetur laud.
+                
+            </p>
+        </div>
+        </div>
+        {/* card 2 */}
+        <div class="max-w-xs mx-8 mb-16 rounded-2xl shadow-lg hover:shadow-2xl hover:scale-105">
+        <img class="w-full h-48 rounded-t-2xl"
+            src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+            alt="product" />
+        <div class="px-6 py-4">
+            <h4 class="mb-3 text-xl font-semibold tracking-tight text-gray-800">This is the title</h4>
+            <p class="leading-normal text-gray-700">Lorem ipsum dolor, sit amet cons ectetur adipis icing
+                elit.
+                Praesen tium, quibusdam facere quo laborum maiores sequi nam tenetur laud.</p>
+        </div>
+        </div>
+        {/* card 3 */}
+        <div class="max-w-xs mx-8 mb-16 rounded-2xl shadow-lg hover:shadow-2xl hover:scale-105">
+        <img class="w-full h-48 rounded-t-2xl"
+            src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+            alt="product" />
+        <div class="px-6 py-4">
+            <h4 class="mb-3 text-xl font-semibold tracking-tight text-gray-800">This is the title</h4>
+            <p class="leading-normal text-gray-700">Lorem ipsum dolor, sit amet cons ectetur adipis icing
+                elit.
+                Praesen tium, quibusdam facere quo laborum maiores sequi nam tenetur laud.</p>
+        </div>
+        </div>
+        {/* card 4 */}
+        <div class="max-w-xs mx-8 mb-16 rounded-2xl shadow-lg hover:shadow-2xl hover:scale-105">
+        <img class="w-full h-48 rounded-t-2xl"
+            src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+            alt="product" />
+        <div class="px-6 py-4">
+            <h4 class="mb-3 text-xl font-semibold tracking-tight text-gray-800">This is the title</h4>
+            <p class="leading-normal text-gray-700">Lorem ipsum dolor, sit amet cons ectetur adipis icing
+                elit.
+                Praesen tium, quibusdam facere quo laborum maiores sequi nam tenetur laud.</p>
+        </div>
+        </div>
+        {/* card 5 */}
+        <div class="max-w-xs mx-8 mb-16 rounded-2xl shadow-lg hover:shadow-2xl hover:scale-105">
+        <img class="w-full h-48 rounded-t-2xl"
+            src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+            alt="product" />
+        <div class="px-6 py-4">
+            <h4 class="mb-3 text-xl font-semibold tracking-tight text-gray-800">This is the title</h4>
+            <p class="leading-normal text-gray-700">Lorem ipsum dolor, sit amet cons ectetur adipis icing
+                elit.
+                Praesen tium, quibusdam facere quo laborum maiores sequi nam tenetur laud.</p>
+        </div>
+        </div>
+        {/* card 6 */}
+        <div class="max-w-xs mx-8 mb-16 rounded-2xl shadow-lg hover:shadow-2xl hover:scale-105">
+        <img class="w-full h-48 rounded-t-2xl"
+            src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+            alt="product" />
+        <div class="px-6 py-4">
+            <h4 class="mb-3 text-xl font-semibold tracking-tight text-gray-800">This is the title</h4>
+            <p class="leading-normal text-gray-700">Lorem ipsum dolor, sit amet cons ectetur adipis icing
+                elit.
+                Praesen tium, quibusdam facere quo laborum maiores sequi nam tenetur laud.</p>
+        </div>
+        </div>
+        {/* card -7 */}
+        <div class="max-w-xs mx-8 mb-16 rounded-2xl shadow-lg hover:shadow-2xl hover:scale-105">
+        <img class="w-full h-48 rounded-t-2xl"
+            src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+            alt="product" />
+        <div class="px-6 py-4">
+            <h4 class="mb-3 text-xl font-semibold tracking-tight text-gray-800">This is the title</h4>
+            <p class="leading-normal text-gray-700">Lorem ipsum dolor, sit amet cons ectetur adipis icing
+                elit.
+                Praesen tium, quibusdam facere quo laborum maiores sequi nam tenetur laud.</p>
+        </div>
+        </div>
+        {/* card 8 */}
+        <div class="max-w-xs mx-8 mb-16 rounded-2xl shadow-lg hover:shadow-2xl hover:scale-105">
+        <img class="w-full h-48 rounded-t-2xl"
+            src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+            alt="product" />
+        <div class="px-6 py-4">
+            <h4 class="mb-3 text-xl font-semibold tracking-tight text-gray-800">This is the title</h4>
+            <p class="leading-normal text-gray-700">Lorem ipsum dolor, sit amet cons ectetur adipis icing
+                elit.
+                Praesen tium, quibusdam facere quo laborum maiores sequi nam tenetur laud.</p>
+        </div>
+        </div>
+        {/* card 9*/}
+        <div class="max-w-xs mx-8 mb-16 rounded-2xl shadow-lg hover:shadow-2xl hover:scale-105">
+        <img class="w-full h-48 rounded-t-2xl"
+            src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+            alt="product" />
+        <div class="px-6 py-4">
+            <h4 class="mb-3 text-xl font-semibold tracking-tight text-gray-800">This is the title</h4>
+            <p class="leading-normal text-gray-700">Lorem ipsum dolor, sit amet cons ectetur adipis icing
+                elit.
+                Praesen tium, quibusdam facere quo laborum maiores sequi nam tenetur laud.</p>
+        </div>
+        </div>
+      </div>
+      
       </Page>
-    </div>
+    </>
   )
 }
