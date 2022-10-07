da66b15eaca6a6294b10dd712510be44f0dd7df6
imsarang <84568747+imsarang@users.noreply.github.com>
2022-10-07 20:13:10 +0530

    navbar updated

--- components/common/Navbar.js
+++ components/common/Navbar.js
@@ -4,7 +4,8 @@ import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import NavbarLink from './NavbarLink'
 import NavbarLinkMobile from './NavbarLinkMobile'
-
+import NavbarRegister from './NavbarRegister'
+import sam from './samarpana.png'
 //import Image from "next/image";
 //import Logo from "../public/streamlineLogo.png";
 
@@ -45,118 +46,122 @@ const Navbar = () => {
 
   const routes = [
     { title: 'Home', path: '/' },
-    { title: 'Events', path: '/events' },
-    { title: 'Announcements', path: '/announcements' },
-    { title: "Samarpana '20", path: '/samarpana-20' },
     { title: 'Registrations', path: '/registrations' },
+    { title: 'Announcements', path: '/announcements' },
+    { title: "Events", path: '/events' },
+    { title: "Samarpana '20", path: '/samarpana-20' },
   ]
 
   return (
-    <nav
-      className="navcontainer"
-      style={{
-        transform: sticky || isOpen ? 'translateY(0%)' : 'translateY(-100%)',
-        transition: 'transform 0.3s ease-in',
-        position: 'sticky',
-        top: '0',
-        zIndex: 99,
-      }}
-    >
-      <div className="w-full">
-        <div className="flex items-center h-16 w-full ">
-          <div className="flex items-center mx-12  justify-between w-full">
-            <div className="">
-              <Link passHref href="https://pes.edu/">
-                <a>
-                  <Image
-                    src="/assets/PES BW AT.svg"
-                    width={140}
-                    height={70}
-                    layout="intrinsic"
-                    alt="PES Logo"
-                    className="invert placeholder-yellow-300 logo "
-                  />
-                </a>
-              </Link>
+    <div>
+      <nav
+        className="navcontainer"
+        style={{
+          transform: sticky || isOpen ? 'translateY(0%)' : 'translateY(-100%)',
+          transition: 'transform 0.3s ease-in',
+          position: 'sticky',
+          top: '0',
+          zIndex: 99,
+        }}
+      >
+        <div className="w-full bg-blue">
+          <div className="flex items-center h-16 w-full ">
+            <div className="flex items-center mx-12 justify-between w-full">
+              <div className="">
+                <Link passHref href="https://pes.edu/">
+                  <a>
+                    <Image
+                      src={sam}
+                      width={120}
+                      height={50}
+                      layout="intrinsic"
+                      alt="PES Logo"
+                      // className="invert placeholder-yellow-300 logo "
+                    />
+                  </a>
+                </Link>
+              </div>
+              <div className="hidden md:block">
+                <div className="ml-10 flex items-baseline space-x-8">
+                  {routes.map((link) => (
+                    <NavbarLink key={link.title} {...link} />
+                  ))}
+                </div>
+              </div>
             </div>
-            <div className="hidden md:block">
-              <div className="ml-10 flex items-baseline space-x-8">
+            <div className="mr-10 flex md:hidden">
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                type="button"
+                className=" inline-flex items-center justify-center p-2 rounded-md text-white   "
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+              >
+                <span className="sr-only">Open main menu</span>
+                {!isOpen ? (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <Transition
+          show={isOpen}
+          enter="transition ease-out duration-300 transform"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100 "
+          leave="transition ease-in duration-200 transform"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          {(ref) => (
+            <div className="md:hidden text-center" id="mobile-menu">
+              <div
+                ref={ref}
+                className=" bg-black px-2 pt-2 pb-3 space-y-1 sm:px-3"
+              >
                 {routes.map((link) => (
-                  <NavbarLink key={link.title} {...link} />
+                  <NavbarLinkMobile key={link.title} {...link} />
                 ))}
               </div>
             </div>
-          </div>
-          <div className="mr-10 flex md:hidden">
-            <button
-              onClick={() => setIsOpen(!isOpen)}
-              type="button"
-              className=" inline-flex items-center justify-center p-2 rounded-md text-white   "
-              aria-controls="mobile-menu"
-              aria-expanded="false"
-            >
-              <span className="sr-only">Open main menu</span>
-              {!isOpen ? (
-                <svg
-                  className="block h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  className="block h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              )}
-            </button>
-          </div>
-        </div>
-      </div>
+          )}
+        </Transition>
+      <NavbarRegister/>
 
-      <Transition
-        show={isOpen}
-        enter="transition ease-out duration-300 transform"
-        enterFrom="opacity-0 scale-95"
-        enterTo="opacity-100 scale-100 "
-        leave="transition ease-in duration-200 transform"
-        leaveFrom="opacity-100 scale-100"
-        leaveTo="opacity-0 scale-95"
-      >
-        {(ref) => (
-          <div className="md:hidden text-center" id="mobile-menu">
-            <div
-              ref={ref}
-              className=" bg-black px-2 pt-2 pb-3 space-y-1 sm:px-3"
-            >
-              {routes.map((link) => (
-                <NavbarLinkMobile key={link.title} {...link} />
-              ))}
-            </div>
-          </div>
-        )}
-      </Transition>
-    </nav>
+      </nav>
+    </div>
   )
 }
 
