ac982151759f3d79a8c73ffc3ddb26d4b352e652
Rohanmehta28 <77066000+Rohanmehta28@users.noreply.github.com>
2022-10-08 00:24:19 +0530

    new web

--- components/common/NavbarRegister.js
+++ components/common/NavbarRegister.js
@@ -6,9 +6,14 @@ import Link from 'next/link'
 const NavbarRegister = () => {
   return (
     <div className='flex justify-between bg-white px-10 py-2'>
-      <Image
-        src={logo}
-        width={130} />
+      <Link passHref href="https://pes.edu/">
+         <a>
+           <Image
+            src={logo}
+            width={130} />
+          </a>
+      </Link>
+     
       <div className='flex justify-center items-center px-2'>
         <Link href='/registrations'>
           <button className='text-req-green border-req-green border-2 py-2 px-8 outline-none rounded-lg text-lg' style={{
