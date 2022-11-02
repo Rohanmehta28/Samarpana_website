3dd6a746cb447eb7645aa0be5971ddba95556fd7
Adithya S K <adithyaskolavi@gmail.com>
2022-11-02 09:54:49 +0530

    donations page added with backend - Adithya S K

---
+++ components/donation/OfflineRegisterForm.js
@@ -0,0 +1,308 @@
+import { addDoc, collection, getFirestore, Timestamp } from 'firebase/firestore'
+import { useCallback, useMemo, useRef, useState } from 'react'
+import { toast } from 'react-toastify'
+import { db } from '../../utils/firebase'
+import getAge from '../../utils/getAge'
+
+/*
+  Registration page
+    - online registration
+      - form
+      - payment -> later
+    - offline reg
+      - admin login 
+      - form -> name of collector
+  */
+export default function OfflineRegisterForm() {
+  const formRef = useRef(null)
+  const [age, setAge] = useState(0)
+
+  const agePriceMap = useMemo(() => {
+    return {
+      olderThan22: {
+        5: 600,
+        10: 750,
+        21: 950,
+      },
+      youngerThan22: {
+        5: 400,
+        10: 550,
+        21: 750,
+      },
+    }
+  }, [])
+
+  const [donator, setDonator] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    pes: true,
+    is_offline_registration: true,
+    dob: '',
+    distance: '5',
+    srn: '',
+    utr: '',
+    collector: '',
+  })
+
+  const updateDetails = useCallback((e) => {
+    setDonator((prev) => {
+      let name = e.target.name
+      let value = e.target.value
+      if (name === 'pes') {
+        value = value === 'Yes'
+      } else if (name === 'dob') {
+        let a = getAge(value)
+        setAge(() => a)
+      }
+      // console.log(name, value, typeof value)
+      return { ...prev, [name]: value }
+    })
+  }, [])
+
+  const registerDonator = useCallback(async () => {
+    toast('Registering...')
+    const p = { ...donator }
+    p.timestamp = Timestamp.fromDate(new Date())
+    let fs
+    if (!db) {
+      fs = getFirestore()
+      await addDoc(collection(fs, 'donator'), p)
+    } else {
+      await addDoc(collection(db, 'donator'), p)
+    }
+    toast('Registered Successfully', { type: 'success' })
+    try {
+      formRef.current.clear()
+    } catch {}
+  }, [donator])
+
+  const validateData = useCallback((donator, age) => {
+    if (
+      donator.name === '' ||
+      donator.email === '' ||
+      donator.phone === '' ||
+      donator.distance === 0 ||
+      donator.distance === '' ||
+      donator.distance === '0' ||
+      donator.collector === '' ||
+      donator.dob === ''
+    ) {
+      toast('Please fill all the fields', { type: 'error' })
+      return false
+    }
+    if (age < 5) {
+      toast('Please enter valid DOB (>5 years)', {
+        type: 'error',
+      })
+      return false
+    }
+    if (
+      !donator.email.match(
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+      )
+    ) {
+      toast('Please enter a valid email address', { type: 'error' })
+      return false
+    }
+
+    // validate phone number
+
+    if (!donator.phone.match(/^[0-9]{10}$/)) {
+      toast('Please enter a valid phone number', { type: 'error' })
+      return false
+    }
+
+    // validate SRN
+    if (donator.pes === true) {
+      if (
+        !(
+          donator.srn.match(/^(pes|PES)[1-2](ug|UG)(18|19|2[0-2])..\d\d\d/) ||
+          donator.srn.match(/^(pes|PES)\d\d\d\d\d\d\d\d\d\d/)
+        )
+      ) {
+        toast('Please enter a valid SRN/ PRN', { type: 'error' })
+        return false
+      }
+    }
+
+    return true
+  }, [])
+  const handleReset = useCallback(() => {
+    setDonator(() => ({
+      name: '',
+      email: '',
+      phone: '',
+      pes: true,
+      is_offline_registration: true,
+      dob: '',
+      distance: '5',
+      srn: '',
+      utr: '',
+      collector: '',
+    }))
+    window?.scrollTo({ top: 0 })
+  }, [])
+
+  const handleRegistration = useCallback(
+    async (e) => {
+      e.preventDefault()
+
+      if (validateData(donator, age)) {
+        try {
+          registerDonator()
+          handleReset()
+        } catch {}
+      } else {
+        try {
+          formRef.current.clear()
+        } catch {}
+      }
+    },
+    [donator, registerDonator, validateData, formRef, age, handleReset]
+  )
+
+  return (
+    <div className="my-10">
+      <form
+        className="flex flex-col text-lg max-w-xl mx-auto gap-4"
+        ref={formRef}
+      >
+        <label className="flex flex-col gap-1">
+          Name
+          <input
+            className="border-2 rounded-md p-1"
+            required
+            type="text"
+            name="name"
+            onChange={updateDetails}
+            value={donator.name}
+          />
+        </label>
+        <label className="flex flex-col gap-1">
+          Email
+          <input
+            className="border-2 rounded-md p-1"
+            required
+            type="email"
+            name="email"
+            onChange={updateDetails}
+            value={donator.email}
+          />
+        </label>
+
+        <label className="flex flex-col gap-1">
+          Phone number
+          <input
+            className="border-2 rounded-md p-1"
+            required
+            type="text"
+            name="phone"
+            onChange={updateDetails}
+            value={donator.phone}
+          />
+        </label>
+
+        <label className="flex flex-col gap-1">
+          Date Of Birth
+          <input
+            className="border-2 rounded-md p-1"
+            required
+            type="date"
+            name="dob"
+            onChange={updateDetails}
+            value={donator.dob}
+          />
+        </label>
+
+        <label className="flex flex-col gap-1">
+          Are you from PES University?
+          <select
+            name="pes"
+            onChange={updateDetails}
+            className="border-2 rounded-md p-2"
+            value={donator.pes ? 'Yes' : 'No'}
+          >
+            <option value="Yes">Yes</option>
+            <option value="No">No</option>
+          </select>
+        </label>
+
+        {donator.pes ? (
+          <>
+            <label className="flex flex-col gap-1">
+              SRN
+              <input
+                name="srn"
+                onChange={updateDetails}
+                type="text"
+                className="border-2 rounded-md p-2"
+                value={donator.srn}
+              />
+            </label>
+          </>
+        ) : (
+          <></>
+        )}
+
+        <label className="flex flex-col gap-1">
+          Marathon Distance
+          <select
+            name="distance"
+            onChange={updateDetails}
+            className="border-2 rounded-md p-2"
+            value={donator.distance}
+          >
+            <option value=""></option>
+            <option value="5">5 Km</option>
+            <option value="10">10 Km</option>
+            <option value="21">21 Km</option>
+          </select>
+        </label>
+
+        <label className="flex flex-col gap-1">
+          Collector
+          <select
+            name="collector"
+            onChange={updateDetails}
+            className="border-2 rounded-md p-2"
+            value={donator.collector}
+          >
+            <option value=""></option>
+            <option value="Akarsh Anil Kumar">Akarsh Anil Kumar</option>
+            <option value="Naitik">Naitik</option>
+            <option value="Soumya">Soumya</option>
+            <option value="Akanksha">Akanksha</option>
+            <option value="Khushi">Khushi</option>
+            <option value="Khushi">Sanath Hoysala</option>
+          </select>
+        </label>
+        <h4 className="font-bold text-step-1 p-2 border-2 border-green-600 rounded-lg text-green-600 my-4">
+          Payment to be done : Rs.
+          {donator.distance && age > 22
+            ? agePriceMap.olderThan22[donator.distance]
+            : agePriceMap.youngerThan22[donator.distance]}
+        </h4>
+        <button
+          className="font-manrope text-[20px] text-black py-2 px-4 rounded-l-full rounded-r-full mt-5"
+          style={{
+            border: '3px solid #5CF3BE',
+          }}
+          onClick={handleRegistration}
+        >
+          Add Runner
+        </button>
+        <button
+          className="font-manrope text-[20px] text-black py-2 px-4 rounded-l-full rounded-r-full mt-5"
+          style={{
+            border: '3px solid #000',
+          }}
+          type="reset"
+          onClick={handleReset}
+        >
+          Clear
+        </button>
+      </form>
+    </div>
+  )
+}
---
+++ components/donation/OtherPaymentDetails.js
@@ -0,0 +1,89 @@
+import Image from 'next/image'
+import { useMemo } from 'react'
+
+function OtherPaymentDetails({ age, distance }) {
+  const agePriceMap = useMemo(() => {
+    return {
+      olderThan22: {
+        5: 'Your Wish',
+        10: 750,
+        21: 950,
+      },
+      youngerThan22: {
+        5: 400,
+        10: 550,
+        21: 750,
+      },
+    }
+  }, [])
+
+  return (
+    <div className="p-4 border-2 border-slate-500 rounded-lg">
+      <h3 className="text-center text-step-1 font-bold">
+        Payment Details for Donators
+      </h3>
+
+      <div className="text-step--1 my-4">
+        <h4 className="font-bold text-step-1 p-2 border-2 border-green-600 rounded-lg text-green-600 my-4">
+          {/* Payment to be done : Rs.
+          {distance && age > 22
+            ? agePriceMap.olderThan22[distance]
+            : agePriceMap.youngerThan22[distance]} */}
+          It’s not the amount that matters but the meaning behind the donation.
+          The efforts of one person can’t move mountains. We would be humbled
+          for any amount of funds donated towards this great cause
+        </h4>
+        <ul>
+          <li className="border-t-2 pt-4">
+            <p className="font-bold text-step-0 my-3">Method 1 : UPI</p>
+            <Image
+              unoptimized
+              src="/assets/PES_UPI.jpeg"
+              alt="QR CODE"
+              layout="responsive"
+              width={727}
+              height={1280}
+            />
+          </li>
+          <li className="border-t-2 pt-4">
+            <p className="font-bold text-step-0 my-3">Method 2 : NEFT/RTGS</p>
+            <p>
+              <b>Name of the Account : </b> PES UNIVERSITY-GENERAL FUND
+            </p>
+            <p>
+              <b>Bank A/c no. : </b> 589301010050057
+            </p>
+            <p>
+              <b>Branch code : </b> 558931
+            </p>
+            <p>
+              <b>Type of Account</b> Current ACCOUNT{' '}
+            </p>
+            <p>
+              <b>Name of Bank & address :</b> Union Bank of India, PESIT branch,
+              100 Feet Ring Road, BSK 3rd Stage, Bangalore-560 085
+            </p>
+            <p>
+              <b>NEFT/IFSC Code of bank : </b> UBIN0558931
+            </p>
+            <p>
+              <b>MICR code of bank : </b> 560026037
+            </p>
+            <p>
+              <b>SWIFT CODE :</b> UBININBBGNB
+            </p>
+            <p>
+              <b>PAN no : </b> AAAJP1445N
+            </p>
+            <p>
+              <b>Contact telephone no : </b> 080-26722448/26722449 Extn.
+              307/505/{' '}
+            </p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default OtherPaymentDetails
